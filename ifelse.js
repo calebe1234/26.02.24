fc2373d75f4695767e8c127c758ed6c60b2b311d
calebe1234 <142435578+calebe1234@users.noreply.github.com>
2024-02-26 12:35:44 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,16 @@
+// Esse código em JavaScript declara uma variável chamada "x" e a inicializa com o valor 2. Isso significa que você pode usar a variável "x" para armazenar e manipular o número 2 em seu programa. Por exemplo, você pode fazer operações matemáticas ou usar esse valor em diversas partes do seu código.//
+var x =2;
+
+// // Esse código em JavaScript declara uma variável chamada "x" e a inicializa com o valor 2. Isso significa que você pode usar a variável "y" para armazenar e manipular o número 2 em seu programa. Por exemplo, você pode fazer operações matemáticas ou usar esse valor em diversas partes do seu código.//
+var y =2;
+
+// Esse trecho de código representa uma condição em JavaScript. Ele verifica se a variável "x" é igual à variável "y". Se a condição for verdadeira, o bloco de código dentro do if será executado; caso contrário, esse bloco será ignorado. Essa estrutura condicional é utilizada para tomar decisões com base na igualdade entre as variáveis "x" e "y".//
+if (x == y) {
+
+    // Este código em JavaScript utiliza `console.log()` para imprimir a mensagem "Números conferem!" no console do navegador ou ambiente de execução JavaScript. Isso é comumente usado para depuração ou para exibir informações durante o desenvolvimento. No entanto, observe que há um pequeno erro de sintaxe: as aspas utilizadas são curvadas (“ ”) em vez de aspas simples ou duplas padrão (' ' ou " "). O código corrigido seria `console.log("Números conferem!");`.//
+
+    console.log("umeros conferem!");
+} else {
+    console.log("umeros conferem!");
+}
+
